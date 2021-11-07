83b1a35ec3f6c80e506658f811f2c87fd7b93f6d
Kevinovitch <tutala@hotmail.fr>
2021-11-08 00:11:48 +0100

    Tuto 07/11/2021

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/task")
+ */
+class TaskController extends AbstractController
+{
+
+    /**
+     * @Route("/new", name="app_task_new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        return $this->render('task/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
